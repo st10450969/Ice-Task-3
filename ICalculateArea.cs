2323ef9788ef52c996c3248646922b9ce117ae33
st10450969 <ST10450969@vcconnect.edu.za>
2025-03-17 13:25:10 +0200

    Added calculate area class

---
+++ ICalculateArea.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ice_Task_3
+{
+    class ICalculateArea
+    {
+    }
+}
--- Rectangle.cs
+++ Rectangle.cs
@@ -6,7 +6,28 @@ using System.Threading.Tasks;
 
 namespace Ice_Task_3
 {
-    class Rectangle
+    public class Rectangle:IShape, ICalculateArea
     {
+        public double Width { get; set; }
+        public double Height { get; set; }
+
+        public Rectangle(string name, double width, double height):base(name)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public double calculateeArea()
+        {
+            return Width * Height;
+        }
+
+        public override void Display()
+        {
+            base.Display();
+            Console.WriteLine("Width: " + Width);
+            Console.WriteLine("Height: " + Height);
+            Console.WriteLine("Area: " + calculateeArea());
+        }
     }
 }
